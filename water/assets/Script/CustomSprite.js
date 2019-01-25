dc76e71de7df737b8f1893bad1774533a3218c91
weidongxuan <weidongxuan@zingfront.com>
2019-01-25 11:08:29 +0800

    add param

--- water/assets/Script/CustomSprite.js
+++ water/assets/Script/CustomSprite.js
@@ -8,37 +8,58 @@ cc.Class({
 
         this.img = new Image();
         this.ctt = new cc.Texture2D();
-        // this.waters = [];
     },
     properties: {
-        metaball: {
-            default: null,
-            type: cc.Texture2D
+        
+        metaballWidth: {
+            default: 200,
+            type: cc.Integer
+        },
+        metaballHeight: {
+            default: 200,
+            type: cc.Integer
         },
         waters: {
             default: null,
             type: cc.Node
+        },
+        _metaball: {
+            default: null,
+            type: cc.Texture2D
         }
     },
     onLoad() {
         cc.director.getPhysicsManager().enabled = true;
+
+        this._metaball = this.spriteFrame.getTexture();
+        this.img.src = this._metaball.url;
     },
-    start() {
-        this.img.src = this.metaball.url;
-    },
+    /**
+     * 绘制canvas => 设置texture
+     */
     changeSpriteFrame() {
+
         let ctx = this.canvas.getContext('2d');
         ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         for (let index = 0; index < this.waters.children.length; index++) {
             const node = this.waters.children[index];
-            ctx.drawImage(this.img, node.x-100, -node.y-100, 200, 200);
+            ctx.drawImage(
+                this.img,
+                node.x - this.metaballWidth / 2,
+                -node.y - this.metaballHeight / 2,
+                this.metaballWidth,
+                this.metaballHeight
+            );
         }
+
         this.ctt.initWithElement(this.canvas);
         this.spriteFrame.setTexture(this.ctt);
     },
+    /**
+     * 实时绘制水珠Metaball，并增加shader
+     */
     update() {
         this.changeSpriteFrame();
         this.node.getComponent("ShaderComponent").updateShader();
-        
     }
 });
--- water/assets/Script/Shader/ShaderComponent.js
+++ water/assets/Script/Shader/ShaderComponent.js
@@ -20,14 +20,7 @@ cc.Class({
 
     start() {
         this._start = Date.now();
-        if (this.target) {
-            let texture = this.target.spriteFrame.getTexture();
-            this._material.setTexture(texture);
-            this._material.setSize(this.target.node.width, this.target.node.height);
-            this._material.updateHash();
-            this.target._material = this._material;
-            this.target._renderData._material = this._material;
-        }
+        this.updateShader();
     },
 
     update(dt) {
